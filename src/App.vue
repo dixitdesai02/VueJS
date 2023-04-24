4e07766c998f442dc85c838298702ef03f4c5a38
Dixit Desai <dixit.d@simformsolutions.com>
2023-04-24 18:25:44 +0530

    Add Navbar as a global component, emits showPrice in app.vue and change button behaviour based on car price availability

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-    <Header />
-    <main class="w-5/6 py-10 max-w-screen-xl m-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
-        <GalleryCard v-for="car in carsData" :car="car" :key="car.title"/>
+    <Navbar />
+    <main class="w-5/6 pt-6 pb-10 max-w-screen-xl m-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+        <GalleryCard @showPrice="showPrice" v-for="car in carsData" :car="car" :key="car.title"/>
     </main>
 </template>
 
 <script>
     import cars from '../cars.json';
     import GalleryCard from "./components/GalleryCard.vue"
-    import Header from "./components/Header.vue"
+    import swal from 'sweetalert';
 
     export default {
         name: "App",
@@ -17,8 +17,18 @@
                 carsData: cars
             }
         },
+        methods: {
+            showPrice(title, price) {
+                swal({
+                    title: title,
+                    text: `Price: ${price}`, 
+                    buttons: {
+                        confirm : {text:'Okay',className:'sweet-ok'},
+                    }
+                })
+            }
+        },
         components: {
-            Header,
             GalleryCard
         }
     }
--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -12,9 +12,10 @@
             <p class="mb-3 font-light text-gray-700 dark:text-gray-400 h-24">
                 {{ car.description?.length > 110 ? car.description.slice(0, 100) + "...": car.description }}
             </p>
-            <button @click="notify" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-slate-600 rounded-lg hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300">
-                Show Price
-                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <button @click="showPrice" :disabled="car.price === undefined" :class="{ disabledBtn: car.price ? false : true }" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-slate-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-slate-300">
+                {{car.price ? 'Show Price': 'Available Soon..'}}
+
+               <svg v-if="car.price" aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
         </div>
     </div>
@@ -22,20 +23,13 @@
 </template>
 
 <script>
-    import swal from 'sweetalert';
-
     export default {
         name: "GalleryCard",
         props: ["car"],
+        emits: ["showPrice"],
         methods: {
-            notify() {
-                swal({
-                    title: this.car.title,
-                    text: `Price: ${this.car.price}`, 
-                    buttons: {
-                        confirm : {text:'Okay',className:'sweet-ok'},
-                    }
-                })
+            showPrice() {
+                this.$emit('showPrice', this.car.title, this.car.price);
             }
         }
     }
@@ -54,4 +48,16 @@
     .wrapper:hover{
         scale: 1.03;
     }
+
+    .disabledBtn {
+        background-color: #cccccc;
+        color: #424242;
+        border: none;
+        cursor: not-allowed;
+    }
+
+    button:hover:enabled {
+        background-color: rgb(51 65 85);
+    }
+
 </style>
