7c56c0c16d76810f52afb52c3b0f4080715b79ca
Dixit Desai <dixit.d@simformsolutions.com>
2023-04-24 10:33:48 +0530

    Complete Practical 1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <Header />
+    <main class="w-5/6 py-10 max-w-screen-xl m-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+        <GalleryCard v-for="car in carsData" :car="car" :key="car.title"/>
+    </main>
+</template>
+
+<script>
+    import cars from '../cars.json';
+    import GalleryCard from "./components/GalleryCard.vue"
+    import Header from "./components/Header.vue"
+
+    export default {
+        name: "App",
+        data() {
+            return {
+                carsData: cars
+            }
+        },
+        components: {
+            Header,
+            GalleryCard
+        }
+    }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="bg-slate-600 text-white text-center pt-5 pb-3 sticky top-0 w-full z-50">
+        <h1 class="text-3xl font-bold">VINTAGE WHEELS</h1>
+        <h3 class="text-md mt-1 text-sky-300 font-semibold">-Retro Reimagined</h3>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
