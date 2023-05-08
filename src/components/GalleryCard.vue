867b7e54fe3e71507b33e3d5faab06519390a8c3
Dixit Desai <dixit.d@simformsolutions.com>
2023-05-08 10:18:47 +0530

    Complete Practical-4: Added Test Api to fetch, edit, delete and update car data

--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="wrapper max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="car-img rounded-t-lg" :src="car.image" alt="" />
-        </a>
+        <div class="overflow-hidden cursor-pointer bg-slate-100">
+            <img class="car-img rounded-t-lg bg-slate-50" :src="car.image" alt="" />
+        </div>
         <div class="p-5">
-            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ car.name.slice(0, 15) }}</h5>
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ car.name?.slice(0, 15) }}</h5>
             <p class="mb-3 font-light text-gray-700 dark:text-gray-400 h-24">
-                {{ car.detail?.length > 110 ? car.detail.slice(0, 100) + "...": car.detail }}
+                {{ car.details?.length > 110 ? car.details?.slice(0, 100) + "...": car.details }}
             </p>
             <div class="flex items-center justify-between">
-                <button @click="showPrice" :disabled="car.price === undefined" :class="{ disabledBtn: car.price ? false : true }" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-slate-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-slate-300">
+                <button @click="showPrice" :disabled="!car.price" :class="{ disabledBtn: car.price ? false : true }" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-slate-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-slate-300">
                     {{car.price ? 'Show Price': 'Available Soon..'}}
                    <svg v-if="car.price" aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
 
                 <div class="icon-wrapper flex justify-between w-[4em] gap-2">
                     <img @click="showEditForm" src="../assets/edit.svg" alt="Edit" class="w-5 h-6 object-contain cursor-pointer opacity-90"/>
-                    <img @click="showDeleteAlert" src="../assets/delete.svg" alt="delete" class="w-6 h-6 cursor-pointer object-contain opacity-80">
+                    <img @click="handleDelete" src="../assets/delete.svg" alt="delete" class="w-6 h-6 cursor-pointer object-contain opacity-80">
                 </div>
             </div>
         </div>
@@ -27,21 +27,44 @@
 <script>
     import ModalForm from './ModalForm.vue';
     import Swal from 'sweetalert2'
+    import axios from 'axios';
 
     export default {
         name: "GalleryCard",
         props: ["car"],
-        emits: ["show-price", "show-form"],
+        emits: ["show-price", "show-form", "update-cars"],
         methods: {
             showPrice() {
                 this.$emit('show-price', this.car.name, this.car.price);
             },
-            showDeleteAlert() {
-                Swal.fire(
-                'Deleted!',
-                `${this.car.name} has been deleted.`,
-                'success'
-                )
+            handleDelete() {
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                    if (result.isConfirmed) {
+                        axios.delete(`https://testapi.io/api/dartya/resource/cardata/${this.car.id}`)
+                        .then(response => {
+                            if (response.status === 204) {
+                                Swal.fire(
+                                'Deleted!',
+                                `${this.car.name} has been Deleted`,
+                                'success'
+                                )
+                                this.$emit('update-cars');
+                            }
+                            else {
+                                alert("Error! Car Could not be Deleted!!")
+                            }
+                        })
+                        .catch(err => { alert("Error! Car Could not be Deleted!!") })   
+                    }
+                })   
             },
             showEditForm() {
                 this.$emit('show-form', {...this.car})
@@ -54,17 +77,26 @@
 </script>
 
 <style scoped>
+    .wrapper {
+        overflow: hidden;
+    }
+
     .car-img {
         width: 100%;
         height: 220px;
         object-fit: cover;
+        transition: all 0.3s ease-in-out;
     }
 
     .wrapper {
         transition: scale ease-in-out 200ms;
     }
     .wrapper:hover{
-        scale: 1.03;
+        scale: 1.02;
+    }
+
+    .wrapper:hover .car-img {
+        scale: 1.1;
     }
 
     .disabledBtn {
