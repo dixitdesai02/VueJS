c15a8c5a728f11774b75d23b652e5c69ad8f2ccc
Dixit Desai <dixit.d@simformsolutions.com>
2023-05-30 15:17:58 +0530

    Fixed width issue of car-image in car details

--- src/views/CarDetails.vue
+++ src/views/CarDetails.vue
@@ -1,15 +1,15 @@
 <template>
         <Loader v-show="isLoading"/>
         <section v-show="!isLoading" class="w-5/6 max-w-6xl m-auto">
-            <div class="back mt-5 cursor-pointer w-fit" @click="goBack">
+            <div class="back mt-5 cursor-pointer" @click="goBack">
                 <img src="/back.svg" alt="back" class="w-10 h-10 object-cover">
             </div>
             <transition name="fade">
                 <div v-show="!isLoading" class="wrapper flex flex-col md:flex-row m-auto items-center bg-white p-5 rounded shadow-lg mt-10">
-                    <div class="img-wrapper">
-                        <img :src="carDetails?.image" alt="Car Image" class="car-image">
+                    <div class="img-wrapper w-full md:w-2/3">
+                        <img :src="carDetails?.image" alt="Car Image" class="car-image object-cover">
                     </div>
-                    <div class="details-wrapper p-5">
+                    <div class="details-wrapper p-5 w-full md:w-1/3">
                         <h1 class="text-3xl font-bold text-slate-800">{{ carDetails?.name }}</h1>
                         <h3 class="text-lg text-slate-600 pt-5 break-words">{{ carDetails?.details }}</h3>
                         <h3 class="text-lg text-slate-800 font-semibold pt-5">$ {{ carDetails?.price }}</h3>
@@ -77,7 +77,7 @@
     @media screen and (max-width: 991px) {
         .car-image {
             width: 100%;
-            height: 200px;   
+            height: 250px;   
             object-fit: cover;
         }
     }
